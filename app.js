a06fee7210eb554b71b8a5bb6af6807ec59d236e
SeanCho <chunghwan14@gmail.com>
2019-11-19 22:14:42 -0500

    kinda working

--- app.js
+++ app.js
@@ -4,39 +4,49 @@
  * @param {String} key 
  *   Your Alpha Vantage API key.
  */
-const alpha = require('alphavantage')({ key: 'qweqweqwe' });
+const alpha = require('alphavantage')({
+  key: '5BXBGO0O397FNY19'
+});
 var util = require('util');
- 
+
 // Simple examples
 // alpha.data.intraday(`ACB.TO`).then(data => {
 //   console.log(data);
 // });
 
 alpha.technical.rsi(`ACB.TO`, `60min`, 14, `close`).then(data => {
-  console.log(data);
+  var data1 = data;
+  // console.log(data); 
+  var jsonRes = {};
+
+  jsonRes = JSON.stringify(data1);
+  
+  console.log(jsonRes);
 });
 
-// var json = data;
+
+
+
 // obj = JSON.parse(json);
 // console.log(obj.count);
 
- 
+
 // alpha.data.batch([`msft`, `aapl`]).then(data => {
 //   console.log(data);
 // });
- 
+
 // alpha.forex.rate('btc', 'usd').then(data => {
 //   console.log(data);
 // });
- 
+
 // alpha.crypto.daily('btc', 'usd').then(data => {
 //   console.log(data);
 // });
- 
+
 // alpha.technical.sma(`msft`, `daily`, 60, `close`).then(data => {
 //   console.log(data);
 // });
- 
+
 // alpha.performance.sector().then(data => {
 //   console.log(data);
 // });
